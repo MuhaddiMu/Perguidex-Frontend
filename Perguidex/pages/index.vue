04d5061afcf5aca6bee3f1ee1a604201b871dc1c
MuhaddiMu <muhaddisshah@gmail.com>
2019-11-18 10:40:13 +0500

    :iphone: Features Create

--- Perguidex/pages/index.vue
+++ Perguidex/pages/index.vue
@@ -45,8 +45,8 @@
                     <v-flex xs12 md3>
                       <v-card flat class="transparent">
                         <v-card-text class="text-center">
-                          <v-icon x-large class="blue--text text--lighten-2"
-                            >mdi-palette</v-icon
+                          <v-icon x-large class="red--text text--darken-1 mb-n4"
+                            >mdi-run</v-icon
                           >
                         </v-card-text>
                         <v-card-title
@@ -54,23 +54,29 @@
                           class="layout justify-center"
                         >
                           <div class="headline text-center">
-                            Material Design
+                            Activities
+                            <v-chip small color="primary">
+                              New
+                            </v-chip>
                           </div>
                         </v-card-title>
-                        <v-card-text>
-                          Cras facilisis mi vitae nunc lobortis pharetra. Nulla
-                          volutpat tincidunt ornare. Pellentesque habitant morbi
-                          tristique senectus et netus et malesuada fames ac
-                          turpis egestas. Nullam in aliquet odio. Aliquam eu est
-                          vitae tellus bibendum tincidunt. Suspendisse potenti.
+                        <v-divider
+                          width="20%"
+                          class="red darken-1 mx-auto"
+                        ></v-divider>
+                        <v-card-text class="text-center">
+                          Keep record of your daily activities which help you
+                          better understand yourself
                         </v-card-text>
                       </v-card>
                     </v-flex>
                     <v-flex xs12 md3>
                       <v-card flat class="transparent">
                         <v-card-text class="text-center">
-                          <v-icon x-large class="blue--text text--lighten-2"
-                            >mdi-palette</v-icon
+                          <v-icon
+                            x-large
+                            class="blue--text text--darken-1 mb-n4"
+                            >mdi-format-list-checkbox</v-icon
                           >
                         </v-card-text>
                         <v-card-title
@@ -78,23 +84,29 @@
                           class="layout justify-center"
                         >
                           <div class="headline text-center">
-                            Material Design
+                            Daily Progress
+                            <v-chip dark small color="pink lighten-1">
+                              Soon
+                            </v-chip>
                           </div>
                         </v-card-title>
-                        <v-card-text>
-                          Cras facilisis mi vitae nunc lobortis pharetra. Nulla
-                          volutpat tincidunt ornare. Pellentesque habitant morbi
-                          tristique senectus et netus et malesuada fames ac
-                          turpis egestas. Nullam in aliquet odio. Aliquam eu est
-                          vitae tellus bibendum tincidunt. Suspendisse potenti.
+                        <v-divider
+                          width="20%"
+                          class="blue darken-1 mx-auto"
+                        ></v-divider>
+                        <v-card-text class="text-center">
+                          Track your everyday progress towards your goals,
+                          objectives and mission
                         </v-card-text>
                       </v-card>
                     </v-flex>
                     <v-flex xs12 md3>
                       <v-card flat class="transparent">
                         <v-card-text class="text-center">
-                          <v-icon x-large class="blue--text text--lighten-2"
-                            >mdi-palette</v-icon
+                          <v-icon
+                            x-large
+                            class="blue-grey--text text--darken-1 mb-n4"
+                            >mdi-chart-timeline-variant</v-icon
                           >
                         </v-card-text>
                         <v-card-title
@@ -102,23 +114,29 @@
                           class="layout justify-center"
                         >
                           <div class="headline text-center">
-                            Material Design
+                            Life Roadmap
+                            <v-chip dark small color="pink lighten-1">
+                              Soon
+                            </v-chip>
                           </div>
                         </v-card-title>
-                        <v-card-text>
-                          Cras facilisis mi vitae nunc lobortis pharetra. Nulla
-                          volutpat tincidunt ornare. Pellentesque habitant morbi
-                          tristique senectus et netus et malesuada fames ac
-                          turpis egestas. Nullam in aliquet odio. Aliquam eu est
-                          vitae tellus bibendum tincidunt. Suspendisse potenti.
+                        <v-divider
+                          width="20%"
+                          class="blue-grey darken-1 mx-auto"
+                        ></v-divider>
+                        <v-card-text class="text-center">
+                          Creating a roadmap will help you get a practical grasp
+                          on what it is going to take
                         </v-card-text>
                       </v-card>
                     </v-flex>
                     <v-flex xs12 md3>
                       <v-card flat class="transparent">
                         <v-card-text class="text-center">
-                          <v-icon x-large class="blue--text text--lighten-2"
-                            >mdi-palette</v-icon
+                          <v-icon
+                            x-large
+                            class="pink--text text--darken-1 mb-n4"
+                            >mdi-cash</v-icon
                           >
                         </v-card-text>
                         <v-card-title
@@ -126,15 +144,259 @@
                           class="layout justify-center"
                         >
                           <div class="headline text-center">
-                            Material Design
+                            Track Cashflow
+                            <v-chip dark small color="pink lighten-1">
+                              Soon
+                            </v-chip>
                           </div>
                         </v-card-title>
-                        <v-card-text>
-                          Cras facilisis mi vitae nunc lobortis pharetra. Nulla
-                          volutpat tincidunt ornare. Pellentesque habitant morbi
-                          tristique senectus et netus et malesuada fames ac
-                          turpis egestas. Nullam in aliquet odio. Aliquam eu est
-                          vitae tellus bibendum tincidunt. Suspendisse potenti.
+                        <v-divider
+                          width="20%"
+                          class="pink darken-1 mx-auto"
+                        ></v-divider>
+                        <v-card-text class="text-center">
+                          Monitoring accurate finance statements can help you
+                          expand your business
+                        </v-card-text>
+                      </v-card>
+                    </v-flex>
+                    <v-flex xs12 md3>
+                      <v-card flat class="transparent">
+                        <v-card-text class="text-center">
+                          <v-icon
+                            x-large
+                            class="purple--text text--darken-1 mb-n4"
+                            >mdi-cash</v-icon
+                          >
+                        </v-card-text>
+                        <v-card-title
+                          primary-title
+                          class="layout justify-center"
+                        >
+                          <div class="headline text-center">
+                            Circles
+                            <v-chip dark small color="pink lighten-1">
+                              Soon
+                            </v-chip>
+                          </div>
+                        </v-card-title>
+                        <v-divider
+                          width="20%"
+                          class="purple darken-1 mx-auto"
+                        ></v-divider>
+                        <v-card-text class="text-center">
+                          Creating a rundown of your connections helps you
+                          improve relationship and circle
+                        </v-card-text>
+                      </v-card>
+                    </v-flex>
+                    <v-flex xs12 md3>
+                      <v-card flat class="transparent">
+                        <v-card-text class="text-center">
+                          <v-icon
+                            x-large
+                            class="yellow--text text--darken-1 mb-n4"
+                            >mdi-lightbulb-on</v-icon
+                          >
+                        </v-card-text>
+                        <v-card-title
+                          primary-title
+                          class="layout justify-center"
+                        >
+                          <div class="headline text-center">
+                            Ideation
+                            <v-chip dark small color="pink lighten-1">
+                              Soon
+                            </v-chip>
+                          </div>
+                        </v-card-title>
+                        <v-divider
+                          width="20%"
+                          class="yellow darken-1 mx-auto"
+                        ></v-divider>
+                        <v-card-text class="text-center">
+                          Write down your ideas daily, structure them in an
+                          intuitive way, share and improve them with time
+                        </v-card-text>
+                      </v-card>
+                    </v-flex>
+                    <v-flex xs12 md3>
+                      <v-card flat class="transparent">
+                        <v-card-text class="text-center">
+                          <v-icon
+                            x-large
+                            class="green--text text--darken-1 mb-n4"
+                            >mdi-emoticon</v-icon
+                          >
+                        </v-card-text>
+                        <v-card-title
+                          primary-title
+                          class="layout justify-center"
+                        >
+                          <div class="headline text-center">
+                            Daily Gratitude
+                            <v-chip dark small color="pink lighten-1">
+                              Soon
+                            </v-chip>
+                          </div>
+                        </v-card-title>
+                        <v-divider
+                          width="20%"
+                          class="green darken-1 mx-auto"
+                        ></v-divider>
+                        <v-card-text class="text-center">
+                          Writing daily gratitude boost happiness. Magic will
+                          unfurl once you begin doing it
+                        </v-card-text>
+                      </v-card>
+                    </v-flex>
+                    <v-flex xs12 md3>
+                      <v-card flat class="transparent">
+                        <v-card-text class="text-center">
+                          <v-icon
+                            x-large
+                            class="amber--text text--darken-1 mb-n4"
+                            >mdi-weight-lifter</v-icon
+                          >
+                        </v-card-text>
+                        <v-card-title
+                          primary-title
+                          class="layout justify-center"
+                        >
+                          <div class="headline text-center">
+                            Fitness
+                            <v-chip dark small color="pink lighten-1">
+                              Soon
+                            </v-chip>
+                          </div>
+                        </v-card-title>
+                        <v-divider
+                          width="20%"
+                          class="amber darken-1 mx-auto"
+                        ></v-divider>
+                        <v-card-text class="text-center">
+                          Keeping your own personal health record can help
+                          improve the quality of care you recieve
+                        </v-card-text>
+                      </v-card>
+                    </v-flex>
+                    <v-flex xs12 md3>
+                      <v-card flat class="transparent">
+                        <v-card-text class="text-center">
+                          <v-icon
+                            x-large
+                            class="brown--text text--darken-1 mb-n4"
+                            >mdi-wardrobe</v-icon
+                          >
+                        </v-card-text>
+                        <v-card-title
+                          primary-title
+                          class="layout justify-center"
+                        >
+                          <div class="headline text-center">
+                            Wardrobe
+                            <v-chip dark small color="pink lighten-1">
+                              Soon
+                            </v-chip>
+                          </div>
+                        </v-card-title>
+                        <v-divider
+                          width="20%"
+                          class="brown darken-1 mx-auto"
+                        ></v-divider>
+                        <v-card-text class="text-center">
+                          Digital wardrobe helps you stay organaized by managing
+                          all of wearings at one place
+                        </v-card-text>
+                      </v-card>
+                    </v-flex>
+                    <v-flex xs12 md3>
+                      <v-card flat class="transparent">
+                        <v-card-text class="text-center">
+                          <v-icon
+                            x-large
+                            class="light-blue--text text--darken-1 mb-n4"
+                            >mdi-clipboard-text</v-icon
+                          >
+                        </v-card-text>
+                        <v-card-title
+                          primary-title
+                          class="layout justify-center"
+                        >
+                          <div class="headline text-center">
+                            Notes
+                            <v-chip dark small color="pink lighten-1">
+                              Soon
+                            </v-chip>
+                          </div>
+                        </v-card-title>
+                        <v-divider
+                          width="20%"
+                          class="light-blue darken-1 mx-auto"
+                        ></v-divider>
+                        <v-card-text class="text-center">
+                          Perguidex allows you to take notes. Access them from
+                          anywhere and share them with your co workers
+                        </v-card-text>
+                      </v-card>
+                    </v-flex>
+                    <v-flex xs12 md3>
+                      <v-card flat class="transparent">
+                        <v-card-text class="text-center">
+                          <v-icon
+                            x-large
+                            class="indigo--text text--darken-1 mb-n4"
+                            >mdi-trophy</v-icon
+                          >
+                        </v-card-text>
+                        <v-card-title
+                          primary-title
+                          class="layout justify-center"
+                        >
+                          <div class="headline text-center">
+                            Achievements
+                            <v-chip dark small color="pink lighten-1">
+                              Soon
+                            </v-chip>
+                          </div>
+                        </v-card-title>
+                        <v-divider
+                          width="20%"
+                          class="indigo darken-1 mx-auto"
+                        ></v-divider>
+                        <v-card-text class="text-center">
+                          Tracking records of your accomplishments helps you set
+                          your personal and professional goals
+                        </v-card-text>
+                      </v-card>
+                    </v-flex>
+                    <v-flex xs12 md3>
+                      <v-card flat class="transparent">
+                        <v-card-text class="text-center">
+                          <v-icon
+                            x-large
+                            class="light-green--text text--darken-1 mb-n4"
+                            >mdi-calendar</v-icon
+                          >
+                        </v-card-text>
+                        <v-card-title
+                          primary-title
+                          class="layout justify-center"
+                        >
+                          <div class="headline text-center">
+                            Calendar
+                            <v-chip dark small color="pink lighten-1">
+                              Soon
+                            </v-chip>
+                          </div>
+                        </v-card-title>
+                        <v-divider
+                          width="20%"
+                          class="light-green darken-1 mx-auto"
+                        ></v-divider>
+                        <v-card-text class="text-center">
+                          Simple online calendar helps you stay organized with
+                          powerful features and customization tools
                         </v-card-text>
                       </v-card>
                     </v-flex>
