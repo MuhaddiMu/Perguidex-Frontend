faef8ea700ac9c2c89ea2490f3b768a7698aee97
MuhaddiMu <muhaddisshah@gmail.com>
2019-11-26 19:54:42 +0500

    :dizzy: Footer Component Create

--- Perguidex/layouts/default.vue
+++ Perguidex/layouts/default.vue
@@ -35,64 +35,17 @@
           <nuxt />
         </v-container>
       </v-content>
-
-      <v-footer class="my-5">
-        <v-container>
-          <v-row no-gutters justify="space-between" align="start">
-            <v-col cols="12" sm="2">
-              <div class="title font-weight-bold mb-5">Product</div>
-              <div class="my-2 grey--text text--darken-2">Get Started</div>
-            </v-col>
-            <v-col cols="12" sm="2">
-              <div class="title font-weight-bold mb-5">Support</div>
-              <div class="my-2 grey--text text--darken-2">Help Center</div>
-              <div class="my-2 grey--text text--darken-2">FAQ</div>
-              <div class="my-2 grey--text text--darken-2">Translations</div>
-            </v-col>
-            <v-col cols="12" sm="2">
-              <div class="title font-weight-bold mb-5">About</div>
-              <div class="my-2 grey--text text--darken-2">About Us</div>
-              <div class="my-2 grey--text text--darken-2">Contact Us</div>
-            </v-col>
-            <v-col cols="12" sm="2">
-              <div class="title font-weight-bold mb-5">Legal</div>
-              <div class="my-2 grey--text text--darken-2">Terms</div>
-              <div class="my-2 grey--text text--darken-2">
-                Privacy
-                <div class="my-2 grey--text text--darken-2">Security</div>
-              </div>
-            </v-col>
-            <v-col cols="12" sm="2">
-              <div>
-                <v-select
-                  value="English"
-                  :items="Lang"
-                  label="Select Language"
-                  outlined
-                ></v-select>
-              </div>
-              <div class="mb-5 text-center">
-                <v-btn icon fab class="grey lighten-2">
-                  <v-icon class="black--text">mdi-facebook</v-icon>
-                </v-btn>
-                <v-btn icon fab class="grey lighten-2">
-                  <v-icon class="black--text">mdi-twitter</v-icon>
-                </v-btn>
-                <v-btn icon fab class="grey lighten-2">
-                  <v-icon class="black--text">mdi-instagram</v-icon>
-                </v-btn>
-              </div>
-            </v-col>
-          </v-row>
-          <!-- <div>&copy; {{ new Date().getFullYear() }}</div> -->
-        </v-container>
-      </v-footer>
+      <TheFooter />
     </v-app>
   </section>
 </template>
 
 <script>
+import TheFooter from '@/components/TheFooter'
 export default {
+  components: {
+    TheFooter
+  },
   data() {
     return {
       Lang: ['English'],
