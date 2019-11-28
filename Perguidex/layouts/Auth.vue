6e057e4958c6a6d9bc4fc03245ba0ef874b22fda
MuhaddiMu <muhaddisshah@gmail.com>
2019-11-28 20:39:48 +0500

    :sparkles: Signup Page Created

---
+++ Perguidex/layouts/Auth.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <v-app light>
+      <v-container fluid>
+        <v-row>
+          <v-col cols="12">
+            <v-row align="center" justify="center">
+              <nuxt />
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-app>
+  </section>
+</template>
---
+++ Perguidex/pages/Signup.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="text-center">
+    <img width="10%" src="Logo.png" class="mb-5" />
+    <v-card max-width="400px" outlined class="mx-auto">
+      <v-card-text>
+        <v-btn tile block depressed
+          ><img width="16px" class="mr-2" src="FacebookIco.png" /> Sign up with
+          Facebook Account</v-btn
+        >
+        <v-btn tile class="mt-2" block depressed
+          ><img width="16px" class="mr-2" src="GoogleIco.png" /> Sign up with
+          Google Account</v-btn
+        >
+        <div class="Half-Seperator caption my-3">OR</div>
+        <v-form ref="Form">
+          <v-text-field
+            outlined
+            label="Full Name"
+            :min="0"
+            prepend-inner-icon="mdi-account"
+          ></v-text-field>
+          <v-text-field
+            outlined
+            label="Email"
+            :min="0"
+            prepend-inner-icon="mdi-email"
+          ></v-text-field>
+          <v-text-field
+            outlined
+            :append-icon="TogglePassword ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="TogglePassword ? 'text' : 'password'"
+            label="Password"
+            :min="0"
+            prepend-inner-icon="mdi-lock"
+            @click:append="TogglePassword = !TogglePassword"
+          ></v-text-field>
+          <div class="caption">
+            By creating an account, you agree to the Perguidex
+            <a href="#" target="_blank">Terms of Service</a> and
+            <a href="#">Privacy Policy</a>.
+          </div>
+          <v-btn text class="red mt-3" dark block>Create My Account</v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'Auth',
+  data() {
+    return {
+      TogglePassword: false
+    }
+  }
+}
+</script>
+
+<style>
+.Half-Seperator {
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: rgba(0, 0, 0, 0.3);
+}
+.Half-Seperator::before,
+.Half-Seperator::after {
+  content: '';
+  flex: 1;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+}
+.Half-Seperator::before {
+  margin-right: 0.25em;
+}
+.Half-Seperator::after {
+  margin-left: 0.25em;
+}
+</style>
