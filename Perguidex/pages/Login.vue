65f6ddaec168ca6f7da74e0102b36d91beb4529b
MuhaddiMu <muhaddisshah@gmail.com>
2019-11-28 21:35:31 +0500

    :art: Login Form

--- Perguidex/pages/Login.vue
+++ Perguidex/pages/Login.vue
@@ -1,8 +1,74 @@
 <template>
-  <h1>Login Page</h1>
+  <div class="text-center">
+    <img width="10%" src="Logo.png" class="mb-5" />
+    <v-card tile max-width="400px" outlined class="mx-auto">
+      <v-card-text>
+        <v-btn tile block depressed
+          ><img width="16px" class="mr-2" src="FacebookIco.png" /> Login with
+          Facebook Account</v-btn
+        >
+        <v-btn tile class="mt-2" block depressed
+          ><img width="16px" class="mr-2" src="GoogleIco.png" /> Login with
+          Google Account</v-btn
+        >
+        <div class="Half-Seperator caption my-3">OR</div>
+        <v-form ref="Form">
+          <v-text-field
+            outlined
+            label="Email"
+            :min="0"
+            prepend-inner-icon="mdi-email"
+          ></v-text-field>
+          <v-text-field
+            outlined
+            :append-icon="TogglePassword ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="TogglePassword ? 'text' : 'password'"
+            label="Password"
+            :min="0"
+            prepend-inner-icon="mdi-lock"
+            @click:append="TogglePassword = !TogglePassword"
+          ></v-text-field>
+          <div class="caption text-left mt-n6 mb-3">
+            <nuxt-link to="Forgot">Forgot Password?</nuxt-link>
+          </div>
+          <v-btn tile text class="red" dark block>Log Me In</v-btn>
+        </v-form>
+        <div class="caption mt-3 text-left">
+          Don't have an account?
+          <nuxt-link to="Signup">Signup here</nuxt-link>
+        </div>
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 <script>
 export default {
-  layout: 'Auth'
+  layout: 'Auth',
+  data() {
+    return {
+      TogglePassword: false
+    }
+  }
 }
 </script>
+
+<style>
+.Half-Seperator {
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: rgba(0, 0, 0, 0.3);
+}
+.Half-Seperator::before,
+.Half-Seperator::after {
+  content: '';
+  flex: 1;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+}
+.Half-Seperator::before {
+  margin-right: 0.25em;
+}
+.Half-Seperator::after {
+  margin-left: 0.25em;
+}
+</style>
