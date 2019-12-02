c0fcb6a44ff76f23c1eec611c2b468d2036f5204
MuhaddiMu <muhaddisshah@gmail.com>
2019-12-02 08:57:47 +0500

    :art: File Structure

---
+++ Perguidex/components/TheHeader.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app-bar
+    class="red lighten-1"
+    elevate-on-scroll
+    :clipped-left="clipped"
+    fixed
+    app
+  >
+    <v-container py-3 pa-0 d-flex>
+      <v-btn
+        depressed
+        nuxt
+        href="/"
+        text
+        class="font-weight-bold text-capitalize white--text"
+      >
+        <v-toolbar-title v-text="'Perguidex'" />
+      </v-btn>
+      <v-spacer />
+      <v-btn nuxt to="Login" text class="d-none d-sm-flex white--text">
+        <div>Login</div>
+      </v-btn>
+
+      <v-btn
+        nuxt
+        to="Signup"
+        depressed
+        class="white red--text font-weight-bold"
+      >
+        <div>Get Started</div>
+      </v-btn>
+    </v-container>
+  </v-app-bar>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      fixed: false
+    }
+  }
+}
+</script>
--- Perguidex/layouts/default.vue
+++ Perguidex/layouts/default.vue
@@ -1,39 +1,7 @@
 <template>
   <section>
     <v-app light>
-      <v-app-bar
-        class="red lighten-1"
-        elevate-on-scroll
-        :clipped-left="clipped"
-        fixed
-        app
-      >
-        <v-container py-3 pa-0 d-flex>
-          <v-btn
-            depressed
-            nuxt
-            href="/"
-            text
-            class="font-weight-bold text-capitalize white--text"
-          >
-            <v-toolbar-title v-text="title" />
-          </v-btn>
-          <v-spacer />
-          <v-btn nuxt to="Login" text class="d-none d-sm-flex white--text">
-            <div>Login</div>
-          </v-btn>
-
-          <v-btn
-            nuxt
-            to="Signup"
-            depressed
-            class="white red--text font-weight-bold"
-          >
-            <div>Get Started</div>
-          </v-btn>
-        </v-container>
-      </v-app-bar>
-
+      <TheHeader />
       <v-content>
         <v-container pa-0 fluid>
           <nuxt />
@@ -46,27 +14,15 @@
 </template>
 
 <script>
+import TheHeader from '@/components/TheHeader'
 import TheFooter from '@/components/TheFooter'
 import ScrollToTop from '@/components/ScrollToTop'
 
 export default {
   components: {
+    TheHeader,
     TheFooter,
     ScrollToTop
-  },
-  data() {
-    return {
-      clipped: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        }
-      ],
-      title: 'Perguidex'
-    }
   }
 }
 </script>
