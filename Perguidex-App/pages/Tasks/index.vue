1c55eaf2505f0c6c61b709e291135d891827c0d2
Muhaddis <muhaddisshah@gmail.com>
2021-05-24 16:33:09 +0500

    :iphone: Fix alignment

--- Perguidex-App/pages/Tasks/index.vue
+++ Perguidex-App/pages/Tasks/index.vue
@@ -1,10 +1,10 @@
 <template>
   <v-container fluid>
     <v-row justify="space-between">
-      <v-col cols="12" sm="8">
+      <v-col cols="12" sm="6">
         <Tasks />
       </v-col>
-      <v-col cols="12" sm="4">
+      <v-col cols="12" sm="5">
         <Quotes />
       </v-col>
     </v-row>
