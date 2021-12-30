0ace494fa527c389c694c4b5b652da8efaf73766
Muhaddis <muhaddisshah@gmail.com>
2021-12-31 03:14:21 +0500

    fix sentry

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,8 +61,19 @@ export default {
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
-    '@nuxtjs/vuetify'
+    '@nuxtjs/vuetify',
+    '@nuxtjs/sentry'
   ],
+
+  sentry: {
+    dsn: process.env.SENTRY_DSN || '', // Enter your project's DSN here
+    // Additional Module Options go here
+    // https://sentry.nuxtjs.org/sentry/options
+    config: {
+      // Add native Sentry config here
+      // https://docs.sentry.io/platforms/javascript/guides/vue/configuration/options/
+    }
+  },
   /*
    ** Nuxt.js modules
    */
